067d3be31c713717eaf96337831d2e3a07622599
Dobriy_Krot <Antoshkin.Alexey.A@gmail.com>
2020-06-22 20:47:41 +0300

    .

--- main.py
+++ main.py
@@ -180,8 +180,11 @@ def continue_chat(message):
         key.add(but_3)
         bot.send_message(message.chat.id, "Выберите:", reply_markup=key)
     elif message.text.find('/all ') != -1 and str(message.chat.id) in const.admins:  # вывод сообщения всем пользователям
-        for user in const.users:
-            bot.send_message(user, message.text[message.text.find('/all ') + 5: len(message.text)])
+        for user in backend.get_users({}):
+            try:
+                bot.send_message(user, message.text[message.text.find('/all ') + 5: len(message.text)])
+            except Exception as err:
+                print('Пользователь удалил чат', err)
     elif message.text.find('/admin') != -1:  # вывод пользователей для админа
         if str(message.chat.id) in const.admins:
             print_users(message.chat.id, 'admin_info_', True)
